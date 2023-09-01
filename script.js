95774f0768c043e23cd84df91913555794b28713
yurilima5 <yurilimarosa12@gmail.com>
2023-08-31 23:10:23 -0300

    second change

--- script.js
+++ script.js
@@ -1,37 +1,120 @@
 //Exercício 1
 
+let tempo = "chuvoso"
+
+if(tempo == "ensolarado"){
+    console.log("O dia está limpo, dá uma praia ;)")
+}else{
+    console.log("o dia está chuvoso, melhor ficar em casa :(")
+}
+
 //Exercício 2
+
+for(let i = 0; i <= 20; i += 2 ){
+   console.log(i)
+}
 
 //Exercício 3
 
+function mensagem(){
+    console.log("Hum hoje podemos comer uma pizza :p")
+}
+ mensagem();
+
 //Exercício 4
+
+function nome (nome){
+    console.log(`meu nome é ${nome}`);
+}
+nome(`Yuri Lima`);
+
 
 //Exercício 5
 
+function sobre (nome , idade , gosto){
+    console.log(`sobre mim ${nome, idade, gosto}`)
+}
+sobre(`Yuri`, `18 anos`, `rock/eletronica` )
+
 //Exercício 6
+
+function midia (filme , musica){
+    console.log(`o filme é ${filme} e a música é ${musica}`)
+}
+midia(`Kungfu panda 3` , `I'm so sorry ~imagine dragons`)
 
 //Exercício 7
 
+//??
+
 //Exercício 8
+
+let clima = limpo = false;
+
+if(clima != true){
+    console.log("o clima é falso");
+}else{
+    console.log("o clima é verídico");
+}
 
 //Exercício 9
 
+let compras = ["sabonete", "shampoo", "cebola", "aipim", "tomate"];
+console.log(compras);  
+
 //Exercício 10
+
+let lista_desejo = ["funko pop Sherek", "Ps5", "mousepad da xuxa", "cabo av"];
+console.log(lista_desejo);
+lista_desejo.unshift("panela polishop");
 
 //Exercício 11
 
+lista_desejo.pop();
+
 //Exercício 12
+
+lista_desejo.push("taça copa do Brasil" , "camisa branca do flamengo")
 
 //Exercício 13
 
+lista_desejo.shift();
+
 //Exercício 14
+
+//??
 
 //Exercício 15
 
+let sobre_yuri = {
+    altura:1.86,
+    personalidade:"extrovertido",
+    comida:"strognoff"
+};
+
 //Exercício 16
+
+sobre_yuri.pet="tem um cão"
 
 //Exercício 17
 
+delete sobre_yuri.comida
+
 //Exercício 18
 
+console.log(sobre_yuri);
+
 //Exercício 19
+
+let cadastro = [
+    {
+        nome: "Kleber",
+        idade: 19,
+        telefone:21985456987,
+        amigos:"jose, carlos, fabiano, jose"
+    }
+]
+console.log(cadastro);
+
+//Exercício 20
+//??
